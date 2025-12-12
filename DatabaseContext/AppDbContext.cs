28691f51babe14c646dec73d4c45d54564efad5f
Kerem Acar <kemracr@gmail.com>
2025-12-12 22:11:57 +0300

    DatabaseContext oluşturuldu ve başlangıç için gerekli tabloları oluşturacak kod yazıld

---
+++ DatabaseContext/AppDbContext.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OpticianWebAPI.Models;
+
+namespace OpticianWebAPI
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {   
+        }
+
+        // Gözlük çerçeve için tablo
+        public DbSet<Frame> Frames {get; set;}
+
+        // Gözlük camı için tablo
+        public DbSet<Lens> Lens {get; set;}
+
+        // Gözlük için tablo
+        public DbSet<Glasses> Glasses {get; set;}
+
+        
+    }
+}
