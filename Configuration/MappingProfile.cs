0f5d66b29e42b96cfc47da3c8088a9450c9e4e60
mortvivant <mehmet.e.aydin0@gmail.com>
2026-01-15 12:35:25 +0300

    mapping profile updated for sales, expenses

--- Configuration/MappingProfile.cs
+++ Configuration/MappingProfile.cs
@@ -21,7 +21,13 @@ namespace OpticianWebAPI.Configuration
             CreateMap<CreateGlassesRequest, Glasses>();
             CreateMap<UpdateGlassesRequest, Glasses>();
             CreateMap<Glasses, GlassesResponse>()
-                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));          
+                .ForMember(dest => dest.Type, opt => opt.MapFrom(src => src.Type.ToString()));
+
+            CreateMap<CreateExpenseRequest , Expenses>();
+            CreateMap<Expenses,ExpenseResponse>();
+
+            CreateMap<CreateSaleRequest, Sales>();
+            CreateMap<Sales,SaleResponse>();          
         }   
     }
 }
